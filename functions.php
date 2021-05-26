ea978b2f59b831e880b93ad79de7420429bf1072
alamin-php <alamin.php100@gmail.com>
2021-05-27 00:13:18 +0600

    add title-tag

--- functions.php
+++ functions.php
@@ -1,1 +1,6 @@
 <?php 
+function alphapro_bootstraping(){
+    load_theme_textdomain( "alphapro");
+    add_theme_support( "title-tag");
+}
+add_action( "after_setup_theme", "alphapro_bootstraping");
--- index.php
+++ index.php
@@ -1,12 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
+<?php get_header(); ?>
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <h1>Hay this is bootstrap</h1>
+        </div>
+    </div>
+</div>
+
+<?php get_footer(); ?>
